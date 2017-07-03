b2013916d3788f109722ae51d3584011d1505337
tyuownu <tyuownu@gmail.com>
2017-07-03 20:48:49 +0800

    feat(quick install): quick install script

---
+++ script/installNeoROS.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# install the neo-ros-pc2 package
+
+CATKIN_WS="$1"
+
+if [ -e "$CATKIN_WS" ]; then
+	echo "Install Neo ros package into $CATKIN_WS"
+else
+	echo "$CATKIN_WS does not exist."
+	echo "Please select an existing initialized Catkin Workspace."
+	exit 1
+fi
+
+cd "$CATKIN_WS"
+
+echo "Installing begin..."
+source devel/setup.bash
+cd src
+git clone https://github.com/micvision/neo-ros-pc2.git
+cd neo-ros-pc2
+rosdep install -a -y -r
+cd ../..
+catkin_make
